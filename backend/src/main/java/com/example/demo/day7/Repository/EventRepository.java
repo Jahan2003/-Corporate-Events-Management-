9feda85c859e6fea58313f3a69477ff66eb7c092
Jahan Sai <91596914+Jahan2003@users.noreply.github.com>
2024-02-29 16:14:29 +0530

    Delete backend/src directory

--- backend/src/main/java/com/example/demo/day7/Repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Model.Event;
-
-public interface EventRepository extends JpaRepository<Event,Long>{
-    
-}
--- backend/src/main/java/com/example/demo/day7/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Model.User;
-import java.util.Optional;
-public interface UserRepository extends JpaRepository<User,Long>{
-    Optional<User> findByEmail(String username);
-}
